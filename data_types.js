4357517d7e15dd3bf3c222d79ce6b3b89716df18
Atimango Mary <atimangomary95@gmail.com>
2024-06-01 12:18:48 +0300

    data types

---
+++ data_types.js
@@ -0,0 +1,46 @@
+let name = 'Atimango Mary';
+let age = 23;
+
+console.log(name, age);
+
+const point = 100;
+console.log(point)
+
+var score =75;
+console.log(score)
+
+// strings
+console.log('hello world');
+let email = 'atimangomary@95gmail.com';
+console.log(email);
+
+let firstName = 'Atimango';
+let lastName = 'Mary';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+console.log(fullName[0]);
+console.log(fullName.length);
+
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
+
+let radius = 10;
+const pi = 3.14;
+console.log(radius, pi);
+
+// math operators +, -, / *, ** %
+
+console.log(10/2);
+// let results = radius % 3;
+ let results = Math.PI * radius**2;
+
+
+// console.log('Math PI:', Math.PI)
+
+// let result = 5* (10-3)**2;
+console.log(results);
