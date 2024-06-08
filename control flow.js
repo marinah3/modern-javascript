0def4160229030c32ce0bc2bd3ebf4611a6aed0c
Atimango Mary <atimangomary95@gmail.com>
2024-06-08 18:07:02 +0800

    project

---
+++ control flow.js
@@ -0,0 +1,46 @@
+// for loops
+
+ //for(let i = 0; i < 5; i++){
+   // console.log('in loop:', i)
+ //}
+
+ //console.log('loop finished');
+
+ //const names = ['shaun', 'mario', 'luigi'];
+
+ //for(let i = 0; i < names.length; i++){
+   // console.log(names[i]);
+  //  let html = `<div>${names[i]}</div>`;
+   // console.log(html);
+
+ //}
+
+
+
+
+ //whlie loops
+// const names = ['shaun', 'mario', 'luigi'];
+ //let i = 0;
+
+ //while( i < 5){
+ //   console.log('in loop:', i);
+   // i++;
+ //}
+
+ //let i = 0;
+ //while(i < names.length){
+   // console.log(names[i]);
+    //i++;
+ //}
+
+
+ // do while loops
+
+ let i = 5;
+
+ do{
+    console.log('val of i is:', i);
+    i++;
+ }while(i < 5);
+
+
---
+++ statements.js
@@ -0,0 +1,108 @@
+// if statements
+//const age = 25;
+
+// if (age > 20){
+//     console.log('you are over 20 years old');
+// }
+
+// const ninjas = ['shaun', 'mario', 'elvis', 'mary'];
+
+// if(ninjas.length > 3){
+//     console.log("that's a lot of ninjas");
+//}
+
+// const password = 'p@ssword';
+//  if (password.length >= 8){
+//     console.log('that password is long enough!')
+//  }
+
+
+//else if statements
+
+// const password = 'p@ssword1234';
+
+// if(password.length >= 12){
+//     console.log('that password is mighty strong')
+// } else if (password.length >= 8){
+//     console.log('that password is long enough!');
+// } else {
+//     console.log('password is not long enough');
+// }
+
+
+
+// logical operators - OR || and AND &&
+// 
+
+
+
+//logical NOT (!)
+
+// let user = false;
+
+// if(user){
+//    console.log('you must be logged in to continue'); 
+
+// }
+
+// console.log(!true);
+// console.log(!false);
+
+
+// break and continue
+
+// const scores = [50, 25, 0, 30, 100, 20, 10];
+
+// for(let i = 0; i < scores.length; i++){
+// if(scores[i] === 0){
+//     continue;
+// }
+// console.log('your score:', scores[i]);
+
+//     if(scores[i] === 100){
+//         console.log('congrats, you got the top score!');
+//         break;
+//     }
+
+// }
+
+
+// //switch statements
+// const grade = '50';
+
+// switch(grade){
+//     case '50':
+//         console.log('you got an A!');
+//         break;
+//     case 'B':
+//         console.log('you got an B!');
+//         break; 
+//     case 'C':
+//         console.log('you got an C!');
+//          break;
+//     case 'D':
+//         console.log('you got an D!');
+//          break;
+//     case 'E':
+//         console.log('you got an E!');
+//          break;
+//         default:
+//             console.log('not a valid grade ');
+// }
+
+
+// variables & block scope
+// let age = 30;
+
+// if(true){
+//     age = 40;
+//     let name = 'mary';
+// console.log("inside 1st code block:", age,name);
+
+// if(true){
+//     let age = 50;
+//     console.log('inside 2nd code block',age)
+// }
+// }
+
+// console.log("outside code block:", age);
