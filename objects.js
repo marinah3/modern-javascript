80f23f64957c8efe6f9a4d955f5378109d2f5ded
Atimango Mary <atimangomary95@gmail.com>
2024-06-28 16:22:28 +0300

    done

--- objects.js
+++ objects.js
@@ -1,19 +1,73 @@
 // object litreals
 
-let user = {
-    name: 'mary',
-    age: 30,
-    email: 'mary@studentatpc.co.ug',
-    location: 'gulu',
-    blog: ['why mac & cheese rules', '10 things to make with marmite']
-};
+// const blogs = [
+//     {title: 'why mac & cheese rules', likes: 30},
+//     {title: '10 things to make with marmite', likes: 50},
+// ];
 
-console.log(user);
-console.log(user .name);
+// console.log(blogs);
 
-user.age = 35;
-console.log(user.age);
+// let user = {
+//     name: 'mary',
+//     age: 30,
+//     email: 'mary@studentatpc.co.ug',
+//     location: 'gulu',
+//     blogs: [{title: 'why mac & cheese rules', likes: 30},
+//     {title: '10 things to make with marmite', likes: 50},
+// ],
+//     login(){
+//         console.log('the user logged in');
+//     },
+//     logout(){
+//         console.log('the user logged out')
+//     },
+//     logBlogs(){
+// //console.log(this.blogs);
+// console.log('this user has written the following blogs:');
+// this.blogs.forEach(blog =>{
+//     console.log(blog.title, blog.likes);
+// })
 
-console.log(user['email']);
-user['name'] = 'elvis';
-console.log(user['name']);
+//     },
+// };
+
+// user.logBlogs();
+// console.log(this);
+
+
+// user.login();
+// user.logout();
+
+// const name = 'mario';
+// name.toUpperCase();
+
+// console.log(user);
+// console.log(user .name);
+
+// user.age = 35;
+// console.log(user.age);
+
+// console.log(user['email']);
+// user['name'] = 'elvis';
+// console.log(user['name']);
+
+//Math Object
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+
+const area = 7.7;
+
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+
+// random numbers
+
+const random = Math.random();
+console.log(random);
+console.log(Math.round(random * 100));
