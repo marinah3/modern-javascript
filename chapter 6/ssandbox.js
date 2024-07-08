7b90f1964e1b7b3505d29c726b8d1b651211e967
Atimango Mary <atimangomary95@gmail.com>
2024-07-08 12:20:17 +0300

    charpter6

--- chapter 6/ssandbox.js
+++ chapter 6/ssandbox.js
@@ -42,10 +42,43 @@
 //});
 
 // Get an update attribute of html element
-const link = document.querySelector('a');
-console.log(link.getAttribute('href'));
-link.setAttribute('href', 'http://www.thenetninja.co.uk');
-link.innerText = 'TheNet Ninja Website'
-const mssg = document.querySelector('p');
-console.log(mssg.getAttribute('class'));
-mssg.setAttribute('class', 'success');
+// const link = document.querySelector('a');
+// console.log(link.getAttribute('href'));
+// link.setAttribute('href', 'http://www.thenetninja.co.uk');
+// link.innerText = 'TheNet Ninja Website'
+// const mssg = document.querySelector('p');
+// console.log(mssg.getAttribute('class'));
+// mssg.setAttribute('class', 'success');
+// mssg.setAttribute('style', 'color: green;')
+
+// How to change style  attribute
+//  const title = document.querySelector('h1');
+//  //title.setAttribute('style', 'margin: 50px');
+// console.log(title.style);
+// console.log(title.style.color);
+// //example.
+// title.style.margin = '50px';
+// title.style.color = 'crimson';
+// title.style.fontSize = '60px'
+// title.style.margin = '';
+
+// How to add or remove other classes
+// const content = document.querySelector('p');
+// console.log(content.classList);
+// content.classList.add('error');
+// content.classList.remove('error');
+// content.classList.add('sucess');
+
+const paras = document.querySelectorAll('p');
+paras.forEach(p => {
+    if(p.textContent.includes('error')){
+        p.classList.add('error');
+    }
+    if(p.innerText.includes('success')){
+        p.classList.add('success');
+    }
+});
+
+const title = document.querySelector('.title');
+title.classList.toggle('test');
+title.classList.toggle('test');
